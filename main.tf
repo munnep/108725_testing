e331b4269d3d543aca47fba826c862f417381dd7
munnep <patrick.munne@hashicorp.com>
2023-12-15 13:27:11 +0100

    added

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "null_resource" "test1" {}
 
 module "submodule1" {
-    source = "./module/submodule1"
+    source = "./modules/submodule1"
 }
 
 module "submodule2" {
-    source = "./module/submodule2"
+    source = "./modules/submodule2"
 }
 
 module "submodule3" {
-    source = "./module/submodule1"
+    source = "./modules/submodule1"
 }
